99b5048cc590032f7d0436971a1d63bc925ef21c
hsshin <hsshin@hsshinui-MacBook-Pro.local>
2018-01-18 15:48:06 +0900

    budget prj init

--- BudgetManager/popup.js
+++ BudgetManager/popup.js
@@ -1,10 +1,12 @@
 $(function(){
-    chrome.storage.sync.get('total', function(budget){
+    chrome.storage.sync.get(['total', 'limit'], function(budget){
         $('#total').text(budget.total);
+        $('#limit').text(budget.limit);
     });
 
+
     $('#spendAmount').click(function(){
-        chrome.storage.sync.get('total', function(budget){
+        chrome.storage.sync.get(['total', 'limit'], function(budget){
             var newTotal = 0;
             if(budget.total){
                 newTotal += parseInt(budget.total);
@@ -15,7 +17,17 @@ $(function(){
                 newTotal += parseInt(amount);
             }
 
-            chrome.storage.sync.set({'total' : newTotal});
+            chrome.storage.sync.set({'total' : newTotal}, function(){
+                if(amount && newTotal >= budget.limit){
+                        var notifOptions = {
+                            type : 'basic',
+                            iconUrl: 'icon48.png',
+                            title: 'Limit reached!',
+                            message : "Uh oh! Looks like you've reach your budget"
+                        };
+                        chrome.notifications.create('limitNotif', notifOptions);
+                }
+            });
 
             $('#total').text(newTotal);
             $('#amount').val('');
